403d68868456996326246def51ee6583ee2ab636
AlexWurster <alex_wurster@hotmail.com>
2025-08-19 20:16:19 +0100

    Small updated on scripts to update the object store file sys representation

--- xaidar/tasks/updateFileSys.py
+++ xaidar/tasks/updateFileSys.py
@@ -1,20 +1,22 @@
+import os
 from pathlib import Path
 
 from xaidar.filesUtils import loadPickle, savePyObj
 from xaidar.s3Utils import decryptCredentials, initialize, iterateObjStore, lstAllKeys
 
-credKey = "v6Q2ZQB7FlFgpI_-8OpC-4g7LZA34JKWesfZsYng7Wg="
+# credKey = "v6Q2ZQB7FlFgpI_-8OpC-4g7LZA34JKWesfZsYng7Wg="
 # credKey = "" #### !!! Must fill this for code to work !!! ####
-# credKey = os.getenv( "CRED_KEY" )
+credKey = os.getenv( "CRED_KEY" )
 credPath =  Path( "./credentials.enc").resolve()
 credDict = decryptCredentials( credKey, credPath )
 client = initialize( "XChem", cred_dict=credDict)
 
-saveDirPath = Path( "./data/s3ObjKeys/xchem/new-raw")
-buckets = ["xchem"]
-for bucket in buckets:
-    iterateObjStore(bucket, client, save = True, function = lstAllKeys, savePath = None, saveDir = "ObjStoreContent", fragSize = 1000, maxLen = None, frag = True, saveObjContent = None)
-print("Done iterating over S3 bucket objects.")
+# # Get a list of all object keys
+# saveDirPath = Path( "./data/s3ObjKeys/xchem/new-raw")
+# buckets = ["xchem"]
+# for bucket in buckets:
+#     iterateObjStore(bucket, client, save = True, function = lstAllKeys, savePath = None, saveDir = "ObjStoreContent", fragSize = 1000, maxLen = None, frag = True, saveObjContent = None)
+# print("Done iterating over S3 bucket objects.")
 
 
 # Get list of all session IDs
@@ -22,7 +24,7 @@ import re
 from collections import defaultdict
 
 dictSessionDirs = defaultdict( set )
-rawXCDir = Path( "../../../data/s3ObjKeys/xchem/new-raw")
+rawXCDir = Path( "./data/s3ObjKeys/xchem/new-raw")
 sessionRegex = "^[a-z][a-z][0-9]+-[0-9]+$"
 
 for fragFile in rawXCDir.iterdir():
